7d1acee30e56d94c96e718bca655b708fb7af09f
rapazz <moises.bravo@rapazz.cl>
2014-07-29 22:57:38 -0400

    Auto instalable
    Se agrega la posibilidad de la auto instalación.

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
-chrome.extension.onRequest.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Agregando botton en GMAIL ' + tab.url + ' GO');
- chrome.tabs.executeScript(null, {file: "contentscript.js"});
 
+chrome.runtime.onInstalled.addListener(function(e){
+console.log(e)
+if (e.reason=="install")
+chrome.tabs.query({}, function(tabs) {
+   
+    for (var i=0;i<=tabs.length-1;i++)
+    {
+    if (tabs[i].url.indexOf("mail.google.com")!=-1)
+    {
+
+    chrome.tabs.executeScript(tabs[i].id, {file: "jquery-2.1.1.min.js"});
+    //cargarScript()
+    chrome.tabs.executeScript(tabs[i].id, {file: "contentscript.js"});
+      
+    }
+      
+    }
 });
+
+//console.log(e)
+})
--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
 
+
+
 var imgURLrrhh = chrome.extension.getURL("images/rrhh.png")
 var imgURLorg = chrome.extension.getURL("images/orgIco.png")
 var imgURLint = chrome.extension.getURL("images/intranet.png")
 
-var a = '<ul class="nav-left list-unstyled"><li class="dropdown"> \
+
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('estilos.css');
+(document.head||document.documentElement).appendChild(style);
+
+
+var a = '<div class="gb_rc gb_e gb_Hc gb_zc"><div class="gb_d gb_e gb_f gb_Hc"> \
+<ul class="nav-left list-unstyled"><li class="dropdown"> \
                 <a href="#" class="gb_c gb_e" name="idkcl" id="idkcl" data-toggle="dropdown"><i class="gb_c gb_e">Aplicaciones KCC</i></a>\
                <div class="dropdown-menu with-arrow panel panel-default admin-options" id="idpanel" ui-not-close-on-click>\
                      <ul class="list-group"> \
@@ -16,13 +27,15 @@ var a = '<ul class="nav-left list-unstyled"><li class="dropdown"> \
                           <li class="gb_l" align="center">\
                           <a  target="_blank"  class="gb_b" href="http://intranet.kcl.cl"><img width="50px" height="50px" align="center" src="'+ imgURLint  +'"><span class="gb_q">Intranet</span></a></li> \
                            </ul></div></li> \
-                           </ul>'
+                           </ul></div></div>'
 
 
 
 
 
-$('.gb_rc.gb_e.gb_Hc.gb_zc:first').before(a)
+//$('.gb_rc.gb_e.gb_Hc.gb_zc:first').before(a)
+$('.gb_qc.gb_Hc.gb_e:first').before(a)
+
 
 
 
@@ -30,10 +43,18 @@ $('.gb_rc.gb_e.gb_Hc.gb_zc:first').before(a)
 if (document.getElementById ('idkcl')!=null)
 {
 
-  
-document.getElementById ('idkcl').addEventListener("click",  function(){
- {
 
+
+document.addEventListener("click",function(){
+
+if (document.getElementById("idpanel").style.display=="block")
+ document.getElementById("idpanel").style.display="none";
+
+})
+  
+document.getElementById ('idkcl').addEventListener("click",  function(e){
+ {
+e.stopPropagation();
 if (document.getElementById("idpanel").style.display!="block")
  document.getElementById("idpanel").style.display="block";
 else 
